1c21b4413a64bf831091a346d44eee45c27ba3a8
Claudio Lisboa <146308330+claudioLisboak@users.noreply.github.com>
2024-01-23 20:29:08 -0300

    ejercicio 2

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -2,3 +2,6 @@ let example = function(a,b,c){
     return a+b+c
 }
 
+let texto = document.querySelector("#text1")
+texto.innerHTML = example(2,2,2)
+
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,3 @@
+const suma = (a,b) => {return a + b}
+let espera = document.querySelector("#text2")
+espera.innerHTML = suma(4,6)
