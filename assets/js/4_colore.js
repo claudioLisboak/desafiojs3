2927749efe1ca789d1943d6364b74a0d442b19d8
Claudio Lisboa <146308330+claudioLisboak@users.noreply.github.com>
2024-01-27 16:58:22 -0300

    fin ejercicio 3

---
+++ assets/js/4_colore.js
@@ -0,0 +1,38 @@
+const div1 = document.getElementById("uno")
+const div2 = document.getElementById("dos")
+const div3 = document.getElementById("tres")
+const div4 = document.getElementById("cuatro")
+
+div1.style.width='200px'
+div1.style.height='200px'
+div1.style.backgroundColor ='blue'
+
+div2.style.width='200px'
+div2.style.height='200px'
+div2.style.backgroundColor ='red'
+
+div3.style.width='200px'
+div3.style.height='200px'
+div3.style.backgroundColor ='green'
+
+div4.style.width='200px'
+div4.style.height='200px'
+div4.style.backgroundColor ='yellow'
+
+const divPadre = document.querySelector(".flex-container")
+divPadre.style.display='flex'
+divPadre.style.gap ='10px'
+
+const pintarDiv1 = () => div1.style.backgroundColor ='black'
+div1.addEventListener("click", pintarDiv1) 
+
+const pintarDiv2 = () => div2.style.backgroundColor ='black'
+div2.addEventListener("click", pintarDiv2)
+
+const pintarDiv3 = () => div3.style.backgroundColor ='black'
+div3.addEventListener("click", pintarDiv3)
+
+const pintarDiv4 = () =>div4.style.backgroundColor='black'
+div4.addEventListener("click", pintarDiv4)
+
+
